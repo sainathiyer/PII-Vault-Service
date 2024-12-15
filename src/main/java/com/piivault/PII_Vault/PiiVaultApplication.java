e3e41b93ad076032087c1a275ba3350fc386c7c2
Sainath Iyer <sainathaniyer@gmail.com>
2024-12-15 18:47:11 +0530

    Added Backend Code

---
+++ src/main/java/com/piivault/PII_Vault/PiiVaultApplication.java
@@ -0,0 +1,13 @@
+package com.piivault.PII_Vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiiVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiiVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piivault/PII_Vault/PiiVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piivault.PII_Vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiiVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
